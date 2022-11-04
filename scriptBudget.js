be774fc72047882c9935d585c838dfbfd519fa02
jacquelynkeller <109810679+jacquelynkeller@users.noreply.github.com>
2022-11-04 13:04:36 -0400

    Update scriptBudget.js

--- scriptBudget.js
+++ scriptBudget.js
@@ -1,8 +1,8 @@
-<script src="https://randojs.com/2.0.0.js"></script>
-<script>
     var count=0;
+    var luckScore=0;
 
-    function copyTextScenario() {
+  function copyTextScenario() 
+    {
         var copyText = document.getElementById("scenario");
 
          // Select the text field
@@ -16,7 +16,8 @@
         alert("Copied the text: " + copyText.value);
     }
 
-     function copyTextEvent() {
+     function copyTextEvent() 
+    {
         var copyText = document.getElementById("event");
 
          // Select the text field
@@ -31,58 +32,77 @@
     }
 
     function getScenario()
-        {
-            var chooseScenario=rando(4);
+    {
+            var chooseScenario=Math.floor(Math.random() * 5);
             var randomScenario;
-  
+    
             switch (chooseScenario) {
-              case 0:
-                randomScenario = "test 1"
-                break;
-               case 1:
-                randomScenario = "test 1"
-                break;
-               case 2:
-                randomScenario = "test 2"
-                break;
-               case 3:
-                randomScenario = "test 3"
-                break;
-               case 4:
-                randomScenario = "test 4"
-                break;
-                
-            const textarea = document.getElementById('scenario');
-            textarea.value = randomScenario;
-        }
+                case 0:
+                    randomScenario = "test 0";
+                    break;
+                case 1:
+                    randomScenario = "test 1";
+                    break;
+                case 2:
+                    randomScenario = "test 2";
+                    break;
+                case 3:
+                    randomScenario = "test 3";
+                    break;
+                case 4:
+                    randomScenario = "test 4";
+                    break;
+            }
+            document.getElementById("scenario").value = randomScenario;
+            document.getElementById("scenarioButton").disabled = true;
+
+    }
   
     
         function getEvent()
         {
-            var chooseEvent[]=randoSequence(5);
-            var currentEvent = chooseEvent[count];
+            var chooseEvent=Math.floor(Math.random() * 5);
             count++;
-            var chooseEvent;
-			
-            switch (chooseScenario) {
-              case 0:
-                chooseEvent = "test 1"
-                break;
+
+            switch (chooseEvent) {
+               case 0:
+                   chooseEvent = "You got a promotion! Your income increased by $50 a month!";
+                   luck=10;
+                   break;
                case 1:
-                chooseEvent = "test 1"
-                break;
+                   chooseEvent = "Your car insurance went up. Vehicle expenses increase by $10 a month.";
+                   luck=-5;
+                   break;
                case 2:
-                chooseEvent = "test 2"
-                break;
+                   chooseEvent = "test 3";
+                   luck=-10;
+                   break;
                case 3:
-                chooseEvent = "test 3"
-                break;
+                   chooseEvent = "test 4";
+                   luck=-40;
+                   break;
                case 4:
-                chooseEvent = "test 4"
-                break;
+                   chooseEvent = "test 5";
+                   luck=30;
+                   break;
+              }
+
             
-            document.getElementById( "event" ).value = chooseEvent;
+
+            if (count==1)
+            {
+              document.getElementById( "event" ).value = chooseEvent;
+              document.getElementById( "luck" ).value=luck;
+            }
+            
+            else
+            {
+                luckScore = document.getElementById( "luck" ).value;
+                luckScore = Number(luckScore) + Number(luck);
+                document.getElementById( "luck" ).value=luckScore;
+                document.getElementById( "event" ).value += "\n" + chooseEvent;
+            }
+            if(count==5)
+              document.getElementById("eventButton").disabled = true;
+
         }
-  
-  
-</script>
