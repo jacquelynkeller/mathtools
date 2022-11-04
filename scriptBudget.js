816409b2ed76ca045652008e78f338e7a0f53cde
jacquelynkeller <109810679+jacquelynkeller@users.noreply.github.com>
2022-11-04 07:59:17 -0400

    Update scriptBudget.js

--- scriptBudget.js
+++ scriptBudget.js
@@ -1,1 +1,30 @@
-
+<script src="https://randojs.com/2.0.0.js"></script>
+<script>
+    function getScenario()
+        {
+            var chooseScenario=rando(4);
+  
+            switch (chooseScenario) {
+              case 0:
+                randomScenario = "test 1"
+                break;
+               case 1:
+                randomScenario = "test 1"
+                break;
+               case 2:
+                randomScenario = "test 2"
+                break;
+               case 3:
+                randomScenario = "test 3"
+                break;
+               case 4:
+                randomScenario = "test 4"
+                break;
+                
+            
+            document.getElementById( "scenario" ).value = randomScenario;
+        }
+  
+  
+  
+</script>
