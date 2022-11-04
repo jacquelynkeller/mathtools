f2990b0a4adb8dcc3e0644b3db9c8178596dc402
jacquelynkeller <109810679+jacquelynkeller@users.noreply.github.com>
2022-11-04 08:16:37 -0400

    Update scriptBudget.js

--- scriptBudget.js
+++ scriptBudget.js
@@ -2,6 +2,21 @@
 <script>
     var count=0;
 
+    function copyTextScenario() {
+        var copyText = document.getElementById("scenario");
+
+         // Select the text field
+         copyText.select();
+         copyText.setSelectionRange(0, 99999); // For mobile devices
+
+         // Copy the text inside the text field
+         navigator.clipboard.writeText(copyText.value);
+
+        // Alert the copied text
+        alert("Copied the text: " + copyText.value);
+    }
+
+
     function getScenario()
         {
             var chooseScenario=rando(4);
